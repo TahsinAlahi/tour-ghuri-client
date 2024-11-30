fe446364b20ffb5c84768cef5b045c8a0de671b8
Tahsin <alahitahsin@gmail.com>
2024-11-30 18:29:40 +0600

    created: Homepage & Header

--- src/RouterRoute.jsx
+++ src/RouterRoute.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import AppLayout from "./layouts/AppLayout";
+import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
 
