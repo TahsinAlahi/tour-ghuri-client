9ecf9fe3333af27f4ccedce94c74b3614f397ee1
Tahsin <alahitahsin@gmail.com>
2024-12-02 20:09:28 +0600

    fixed: HomeTouristSopts responsive

--- src/components/HomeTouristSpots.jsx
+++ src/components/HomeTouristSpots.jsx
@@ -6,7 +6,7 @@ function HomeTouristSpots() {
       <h1 className="text-5xl font-bold text-center mb-10 pt-10 border-b-2 border-title-underline w-fit mx-auto font-nunito">
         Tourist Spots
       </h1>
-      <div className="grid grid-cols-3 max-w-screen-lg mx-auto min-h-[50svh] w-full rounded-lg gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4 max-w-screen-lg mx-auto min-h-[50svh] w-full rounded-lg gap-2">
         <HomeTouristCard />
         <HomeTouristCard />
         <HomeTouristCard />
