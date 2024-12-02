bbd19a80a802e3a128034826eec08cd2919c7d0f
Tahsin <alahitahsin@gmail.com>
2024-12-03 04:03:18 +0600

    updated: added user detection

--- src/providers/AuthContext.jsx
+++ src/providers/AuthContext.jsx
@@ -1,8 +1,25 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase.config";
+import { onAuthStateChanged } from "firebase/auth";
 
 const authContext = createContext(null);
 
 function AuthContext({ children }) {
+  const [authLoading, setAuthLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setAuthLoading(true);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setAuthLoading(false);
+    });
+
+    console.log(user);
+
+    return () => unsubscribe();
+  }, []);
+
   const value = {};
   return <authContext.Provider value={value}>{children}</authContext.Provider>;
 }
