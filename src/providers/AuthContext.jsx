6a476742978eb6d396d574201db761449320de07
Tahsin <alahitahsin@gmail.com>
2024-12-03 04:32:17 +0600

    updated: registerWithEmail

--- src/providers/AuthContext.jsx
+++ src/providers/AuthContext.jsx
@@ -1,12 +1,48 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "../firebase.config";
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
 
 const authContext = createContext(null);
 
 function AuthContext({ children }) {
   const [authLoading, setAuthLoading] = useState(true);
   const [user, setUser] = useState(null);
+
+  async function registerWithEmail(email, password, displayName, imageUrl) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      await updateProfile(userCredential.user, {
+        displayName: displayName,
+        photoURL: imageUrl,
+      });
+
+      setUser(userCredential.user);
+      return { status: "success", message: "User created successfully" };
+    } catch (error) {
+      console.log(error);
+      let errorMessage = "An error occurred. Please try again.";
+      if (error.code === "auth/email-already-in-use") {
+        errorMessage = "This email is already in use.";
+      } else if (error.code === "auth/invalid-email") {
+        errorMessage = "The email address is invalid.";
+      } else if (error.code === "auth/weak-password") {
+        errorMessage = "The password is too weak.";
+      } else if (error.code === "auth/network-request-failed") {
+        errorMessage = "Network error. Please check your connection.";
+      }
+
+      return { status: "error", message: errorMessage };
+    }
+  }
 
   useEffect(() => {
     setAuthLoading(true);
@@ -20,7 +56,8 @@ function AuthContext({ children }) {
     return () => unsubscribe();
   }, []);
 
-  const value = {};
+  const value = { user, registerWithEmail };
+
   return <authContext.Provider value={value}>{children}</authContext.Provider>;
 }
 
